121023c547313ad43c3a5d119ac6b9fd163ccf8c
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-30 09:03:42 -0700

    connect to SQL database, add tables, run first test that requires the DB and pass

---
+++ app.rb
@@ -0,0 +1,1 @@
+DB = PG.connect({:dbname => 'volunteer_tracker'})
--- lib/project.rb
+++ lib/project.rb
@@ -6,4 +6,23 @@ class Project
     attributes.each {|pair| instance_variable_set("@#{pair[0].to_s}", pair[1])}
   end
 
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |projects|
+      name = project.fetch("name")
+      id = project.fetch("id").to_i
+      project.push(Album.new({ :title => title, :id => id}))
+    end
+    project
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first.fetch('id').to_i
+  end
+
+
+
+
 end
