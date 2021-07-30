30b39368b8f252ebfba449b56c58960e2e0dc7d8
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-30 09:48:11 -0700

    pass test for blank volunteer return and volunteers being equal

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,3 +1,25 @@
 class Volunteer
+  attr_accessor :name, :project_id
+  attr_reader :id
+
+  def initialize(attributes)
+    attributes.each {|pair| instance_variable_set("@#{pair[0].to_s}", pair[1])}
+  end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      project_id = volunteer.fetch("project_id").to_i
+      volunteers.push(Volunteer.new({ :name => name, :id => id, :project_id => project_id }))
+    end
+    volunteers
+  end
+
+  def ==(vol_to_compare)
+    self.name() == vol_to_compare.name()
+  end
 
 end
