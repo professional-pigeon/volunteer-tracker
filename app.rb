50455d4e063d2eca6f4d2e45223aaf7119c6b1cb
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-30 10:28:01 -0700

    make rough erb layout add a background

--- app.rb
+++ app.rb
@@ -1,1 +1,14 @@
+require "sinatra"
+require "sinatra/reloader"
+also_reload "lib/**/*.rb"
+require "pry"
+require "pg"
+require "./lib/project"
+require "./lib/volunteer"
+
 DB = PG.connect({:dbname => 'volunteer_tracker'})
+
+get('/') do 
+  @projects = Project.all
+  erb(:main)
+end
