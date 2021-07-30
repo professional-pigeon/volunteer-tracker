6c1d467993f59f04316b3f2d3d62096442513373
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-30 14:32:16 -0700

    pass all capybara tests

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -57,7 +57,7 @@ describe 'the volunteer detail page path', {:type => :feature} do
     project_id = test_project.id.to_i
     test_volunteer = Volunteer.new({:name => 'Jasmine', :project_id => project_id, :id => nil})
     test_volunteer.save
-    visit "/projects/#{project_id}"
+    visit "/project/#{project_id}"
     click_link('Jasmine')
     fill_in('name', :with => 'Jane')
     click_button('Update Volunteer')
