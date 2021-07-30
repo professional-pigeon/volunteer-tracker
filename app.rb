98e4dc78b5b9c7c111cfbf0d470703b09acd9d57
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-30 11:05:39 -0700

    create pathway for looking at an individual project, post request logic for adding a project, and pass first capybara test.

--- app.rb
+++ app.rb
@@ -12,3 +12,14 @@ get('/') do
   @projects = Project.all
   erb(:main)
 end
+
+post('/projects') do
+  proj_obj = Project.new({:title => params[:project_title], :id => nil})
+  proj_obj.save
+  redirect to ('/')
+end
+
+get('/project/:id') do
+  @project = Project.find(params[:id].to_i)
+  erb(:project)
+end
