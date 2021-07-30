6742b2018dc78e8b66119cd1590b99f7280cf8ae
professional-pigeon <kyle.kayperez@gmail.com>
2021-07-30 11:58:49 -0700

    pass third capybara test

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -41,7 +41,7 @@ describe 'the project delete path', {:type => :feature} do
     test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
     test_project.save
     id = test_project.id
-    visit "/projects/#{id}/edit"
+    visit "/project/#{id}"
     click_button('Delete Project')
     visit '/'
     expect(page).not_to have_content("Teaching Kids to Code")
